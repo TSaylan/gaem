37fe2948ba88d68f4391234d20df13a34b5b7e2d
TSaylan <78858344+TSaylan@users.noreply.github.com>
2023-09-01 09:15:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,70 +1,30 @@
-import pygame
+import game
+import fileStuff
 
 # display
-pygame.init()
-surface = pygame.display.set_mode((800, 800))
-pygame.display.set_caption("gaem")
+game.pygame.init()
+surface = game.pygame.display.set_mode(size=(800, 800))
+game.pygame.display.set_caption(title="gaem")
 
 # load
-try:
-    with open("save.txt", "rt") as file:
-        line = file.readlines()
-
-except FileNotFoundError:
-    with open("save.txt", "wt") as file:
-        file.writelines("400\n400\n")
-    with open("save.txt", "rt") as file:
-        line = file.readlines()
-
-while len(line) < 2:
-    line.append("400\n")
-
-for i in range(len(line)):
-    line[i] = line[i].strip()
-    if not line[i]:
-        line[i] = 0
-
-# make player
-player = pygame.Vector2(int(line[0]), int(line[1]))
-playerRect = pygame.Rect(0, 0, 100, 100)
+line = fileStuff.load(file="save.txt")
 
 # game loop
-clock = pygame.time.Clock()
+clock = game.pygame.time.Clock()
 deltaTime = 0
 RUNNING = True
 while RUNNING:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            RUNNING = False
-            
-    # player movement
-    key = pygame.key.get_pressed()
-    if key[pygame.K_w]:
-        player.y -= 300 * deltaTime
-    if key[pygame.K_s]:
-        player.y += 300 * deltaTime
-    if key[pygame.K_a]:
-        player.x -= 300 * deltaTime
-    if key[pygame.K_d]:
-        player.x += 300 * deltaTime
-
-    playerRect.center = player
+    for event in game.pygame.event.get():
+        if event.type == game.pygame.QUIT:
+            RUNNING = False     
 
     # draw stuff    
-    surface.fill((30, 30, 30))
-
-    pygame.draw.rect(surface, (255, 69, 0), playerRect)
+    surface.fill(color=(30, 30, 30))
 
     # update surface
-    pygame.display.flip()
-    deltaTime = clock.tick(90) / 1000
+    game.pygame.display.flip()
+    deltaTime = clock.tick(framerate=90) / 1000
 
 # save and quit
-line[0] = int(player.x)
-line[1] = int(player.y)
-
-with open("save.txt", "wt") as file:
-    for i in range(len(line)):
-        file.writelines(str(line[i]) + "\n")
-            
-pygame.quit()
+fileStuff.save(file="save.txt", line=line)
+game.pygame.quit()
