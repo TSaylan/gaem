16923570b706b163f63016da5de3a8395084f92f
TSaylan <78858344+TSaylan@users.noreply.github.com>
2023-09-01 09:33:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,30 @@
-import game
+import pygame
 import fileStuff
 
 # display
-game.pygame.init()
-surface = game.pygame.display.set_mode(size=(800, 800))
-game.pygame.display.set_caption(title="gaem")
+pygame.init()
+surface = pygame.display.set_mode(size=(800, 800))
+pygame.display.set_caption(title="gaem")
 
 # load
 line = fileStuff.load(file="save.txt")
 
 # game loop
-clock = game.pygame.time.Clock()
+clock = pygame.time.Clock()
 deltaTime = 0
 RUNNING = True
 while RUNNING:
-    for event in game.pygame.event.get():
-        if event.type == game.pygame.QUIT:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             RUNNING = False     
 
     # draw stuff    
     surface.fill(color=(30, 30, 30))
 
     # update surface
-    game.pygame.display.flip()
+    pygame.display.flip()
     deltaTime = clock.tick(framerate=90) / 1000
 
 # save and quit
 fileStuff.save(file="save.txt", line=line)
-game.pygame.quit()
+pygame.quit()
